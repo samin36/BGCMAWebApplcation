66e220ddd1c21946a169be58be1ca7b10a601410
Shrey Amin <samin36@gatech.edu>
2020-08-20 22:53:42 -0400

    auto update

--- src/Components/ApplicationEntries.js
+++ src/Components/ApplicationEntries.js
@@ -1,8 +1,9 @@
 import _ from "lodash";
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Table, Icon, Button } from "semantic-ui-react";
 import ApplicationEntry from "./ApplicationEntry";
 import { UserDataContext } from "../Context/UserDataContext";
+import NewApplication from "./Forms/NewApplication";
 
 const dataReducer = (state, action) => {
   switch (action.type) {
@@ -36,6 +37,7 @@ const dataReducer = (state, action) => {
 
 const ApplicationEntries = () => {
   const { userData, setUserData } = useContext(UserDataContext);
+  const [showForm, setShowForm] = useState(false);
   const [state, dispatch] = React.useReducer(dataReducer, {
     column: null,
     direction: null,
@@ -45,56 +47,68 @@ const ApplicationEntries = () => {
     setUserData(state.data);
   }, [state.data, setUserData]);
 
+  const createNewApplication = () => {
+    console.log("here");
+    setShowForm(true);
+  };
+
   return (
-    <Table
-      striped
-      size="large"
-      padded="very"
-      singleLine
-      selectable
-      sortable
-      textAlign="center"
-      color="blue"
-      style={{
-        marginTop: "-0.8em",
-        background: "#F8F8FF",
-        fontSize: "1.25em",
-      }}
-    >
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell
-            onClick={() => dispatch({ type: "SORT", column: "name" })}
-          >
-            Child Name
-            <Icon name="sort" />
-          </Table.HeaderCell>
-          <Table.HeaderCell>Actions</Table.HeaderCell>
-          <Table.HeaderCell
-            onClick={() => dispatch({ type: "SORT", column: "date" })}
-          >
-            Date
-            <Icon name="sort" />
-          </Table.HeaderCell>
-          <Table.HeaderCell>Application Status</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {userData &&
-          userData.map((child, index) => (
-            <ApplicationEntry key={index} child={child} dispatch={dispatch} />
-          ))}
-      </Table.Body>
-      <Table.Footer fullWidth>
-        <Table.Row textAlign="center">
-          <Table.HeaderCell colSpan="4">
-            <Button color="facebook" circular>
-              New Application
-            </Button>
-          </Table.HeaderCell>
-        </Table.Row>
-      </Table.Footer>
-    </Table>
+    <>
+      <Table
+        striped
+        size="large"
+        padded="very"
+        singleLine
+        selectable
+        sortable
+        textAlign="center"
+        color="blue"
+        style={{
+          marginTop: "-0.8em",
+          background: "#F8F8FF",
+          fontSize: "1.25em",
+        }}
+      >
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell
+              onClick={() => dispatch({ type: "SORT", column: "name" })}
+            >
+              Child Name
+              <Icon name="sort" />
+            </Table.HeaderCell>
+            <Table.HeaderCell>Actions</Table.HeaderCell>
+            <Table.HeaderCell
+              onClick={() => dispatch({ type: "SORT", column: "date" })}
+            >
+              Date
+              <Icon name="sort" />
+            </Table.HeaderCell>
+            <Table.HeaderCell>Application Status</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {userData &&
+            userData.map((child, index) => (
+              <ApplicationEntry key={index} child={child} dispatch={dispatch} />
+            ))}
+        </Table.Body>
+        <Table.Footer fullWidth>
+          <Table.Row textAlign="center">
+            <Table.HeaderCell colSpan="4">
+              <Button
+                color="facebook"
+                circular
+                onClick={() => createNewApplication()}
+              >
+                New Application
+              </Button>
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Footer>
+      </Table>
+      {showForm && <NewApplication />}
+    </>
   );
 };
 
