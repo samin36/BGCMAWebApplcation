2fe00c6857f2c04c02e70a8b6fa0f70ba2c08264
pnamani3 <pnamani3@gatech.edu>
2020-09-01 23:23:01 -0400

    implementing page12

--- src/Components/Forms/Page12.js
+++ src/Components/Forms/Page12.js
@@ -1,23 +1,32 @@
 import React from "react";
-import { Form, Container, Icon, Header } from "semantic-ui-react";
+import {
+  Form,
+  Container,
+  Icon,
+  Header,
+  Segment,
+  Grid,
+  Divider,
+} from "semantic-ui-react";
 import * as yup from "yup";
 import { Formik } from "formik";
 import { Persist } from "formik-persist";
-import { dateRegex } from "../../Regex/regex";
+import {
+  //phoneRegex,
+  ssnRegex,
+  dateRegex,
+  //zipCodeRegex,
+} from "../../Regex/regex";
 import {
   paragraph1,
-  list1,
-  list2,
-  list3,
-  list4,
-  list5,
-  signing,
-} from "../../PageText/page11text";
+  hr
+} from "../../PageText/page12text";
 /**
  * Pass in prevStep if the page number >= 1
+ * Notes: <hr> to horizonatal line
  */
 const FormTemplate = ({ nextStep, prevStep, setFormStates }) => {
-  const pageNo = 11; //Define the page number here
+  const pageNo = 12; //Define the page number here
   const updateFormState = (values) => {
     setFormStates((prevState) => {
       return {
@@ -38,19 +47,47 @@ const FormTemplate = ({ nextStep, prevStep, setFormStates }) => {
   };
 
   const initialValues = {
-      parentSign: "",
-      childName: "",
-      signatureDate:"",
+      youthLastName: "",
+      youthFirstName:"",
+      youthMI: "",
+      youthSSN: "",
+      youthGender: "",
+      documentDate: "",
+      youthFosterCare: "",
+      youthFosterCareEntry: "",
+      youthUSCiti: "",
+      youthGARes: "",
+      youth3CateQuestion: "",
+      youth3CateRadio: "",
+      ATANF: "",
+      BSNAP: "",
+      CSSI: "",
+      DPFK: "",
   };
 
-  const validationSchema = yup.object().shape({
-      parentSign: yup.string().required("Parent Signature/ Name is required."),
-      signatureDate: yup
-      .string()
-      .required("Date is required as MM/DD/YYYY.")
-      .matches(dateRegex, "Date must be in the form MM/DD/YYYY."),
-      childName: yup.string().required("Child Name is required."),
+  const section1ValidationSchema = yup.object().shape({
+      youthLastName : yup.string().required("Child's Last Name is required"),
+      youthFirstName: yup.string().required("Child's First Name is required"),
+      youthMI :  yup.string().max(1, "Initial must be a single letter"),
+      youthSSN : yup.string().required("Child's SSN is required").matches(ssnRegex, "SSN must be in the form xxx-xx-xxxx"),
+      youthGender: yup.string().required("Gender must be selected"),
+      documentDate: yup.string().required("Date is required as MM/DD/YYYY").matches(dateRegex,"Date must be in the form MM/DD/YYYY"),
+      youthFosterCareEntry : yup.string(),
+      youthFosterCare: yup.string().required("You must select either Yes or No."),
   });
+
+  const section2ValidationSchema = yup.object().shape({
+      youthUSCiti : yup.string().required("You must select either Yes or No."),
+      youthGARes : yup.string().required("You must select either Yes or No."),
+      youth3CateQuestion: yup.string().required("You must select either Yes or No."),
+      youth3CateRadio: yup.string().required("You must select one of these 3 options."),
+      ATANF : yup.string().required("You must select either Yes or No."),
+      BSNAP : yup.string().required("You must select either Yes or No."),
+      CSSI : yup.string().required("You must select either Yes or No."),
+      DPFK : yup.string().required("You must select either Yes or No."),
+  });
+
+  const validationSchema = yup.object().concat(section1ValidationSchema).concat(section2ValidationSchema);
 
   return (
     <Formik
@@ -86,61 +123,186 @@ const FormTemplate = ({ nextStep, prevStep, setFormStates }) => {
           </Header>
           <Header as="h4" textAlign="left">
             {paragraph1}
-            {list1}
-            {list2}
-            {list3}
-            {list4}
-            {list5}
-            {signing}
+            {hr}
           </Header>
           <Form size="big">
             <Form.Group widths="equal">
               <Form.Input
                 icon={<Icon name="asterisk" size="small" color="red" />}
                 error={
-                  touched.parentSign &&
-                  errors.parentSign !== undefined && {
-                    content: errors.parentSign,
+                  touched.youthLastName &&
+                  errors.youthLastName !== undefined && {
+                    content: errors.youthLastName,
                     pointing: "above",
                   }
                 }
-                placeholder="Parent Signature/ Name"
-                name="parentSign"
-                value={values.parentSign}
+                placeholder="Youth Last Name"
+                name="youthLastName"
+                value={values.youthLastName}
                 onChange={handleChange}
                 onBlur={handleBlur}
               />
               <Form.Input
                 icon={<Icon name="asterisk" size="small" color="red" />}
                 error={
-                  touched.childName &&
-                  errors.childName !== undefined && {
-                    content: errors.childName,
+                  touched.youthFirstName &&
+                  errors.youthFirstName !== undefined && {
+                    content: errors.youthFirstName,
                     pointing: "above",
                   }
                 }
-                placeholder="Youth Name"
-                name="childName"
-                value={values.childName}
+                placeholder="Youth First Name"
+                name="youthFirstName"
+                value={values.youthFirstName}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              <Form.Input
+                // icon={<Icon name="asterisk" size="small" color="red" />}
+                error={
+                  touched.youthMI &&
+                  errors.youthMI !== undefined && {
+                    content: errors.youthMI,
+                    pointing: "above",
+                  }
+                }
+                placeholder="Youth Middle Initial"
+                name="youthMI"
+                value={values.youthMI}
                 onChange={handleChange}
                 onBlur={handleBlur}
               />
             </Form.Group>              
-              <Form.Input
+            <Form.Group>
+             <Form.Input
                 icon={<Icon name="asterisk" size="small" color="red" />}
                 error={
-                  touched.signatureDate &&
-                  errors.signatureDate !== undefined && {
-                    content: errors.signatureDate,
+                  touched.youthSSN &&
+                  errors.youthSSN !== undefined && {
+                    content: errors.youthSSN,
                     pointing: "above",
                   }
                 }
-                placeholder="Date"
-                name="signatureDate"
-                value={values.signatureDate}
+                placeholder="SSN (Youth)"
+                name="youthSSN"
+                value={values.youthSSN}
                 onChange={handleChange}
                 onBlur={handleBlur}
               />
+              <Form.Group grouped>
+                      <label>Gender: </label>
+                      <Icon name="asterisk" color="red" size="small" corner />
+                      <Form.Radio
+                        name="youthGender"
+                        value="Female"
+                        label="Female"
+                        onBlur={handleBlur}
+                        onChange={(_, { value }) =>
+                          setFieldValue("youthGender", value)
+                        }
+                        checked={values.youthGender === "Female"}
+                        error={
+                          touched.youthGender &&
+                          errors.youthGender !== undefined
+                        }
+                      />
+                      <Form.Radio
+                        name="youthGender"
+                        value="Male"
+                        label="Male"
+                        onBlur={handleBlur}
+                        onChange={(_, { value }) =>
+                          setFieldValue("youthGender", value)
+                        }
+                        checked={values.youthGender === "Male"}
+                        error={
+                          touched.youthGender &&
+                          errors.youthGender !== undefined
+                        }
+                      />
+                      <Form.Radio
+                        name="youthGender"
+                        value="Non-Binary"
+                        label="Non-Binary"
+                        onBlur={handleBlur}
+                        onChange={(_, { value }) =>
+                          setFieldValue("youthGender", value)
+                        }
+                        checked={values.childGender === "Non-Binary"}
+                        error={
+                          touched.youthGender &&
+                          errors.youthGender !== undefined && {
+                            content: errors.youthGender,
+                            pointing: "above",
+                          }
+                        }
+                      />
+                    </Form.Group>
+            
+            <Form.Input
+                icon={<Icon name="asterisk" size="small" color="red" />}
+                error={
+                  touched.documentDate &&
+                  errors.documentDate !== undefined && {
+                    content: errors.documentDate,
+                    pointing: "above",
+                  }
+                }
+                placeholder="Date (MM/DD/YYYY)"
+                name="documentDate"
+                value={values.documentDate}
+                onChange={handleChange}
+                onBlur={handleBlur}
+            />
+            </Form.Group>
+            <Form.Group grouped>
+                      <b> Is the youth named above in Foster Care within the state of Georgia?</b>
+                      <Icon name="asterisk" color="red" size="small" corner />
+                      <Form.Radio
+                        name="youthFosterCare"
+                        value="Yes"
+                        label="Yes"
+                        onBlur={handleBlur}
+                        onChange={(_, { value }) =>
+                          setFieldValue("youthFosterCare", value)
+                        }
+                        checked={values.youthFosterCare === "Yes"}
+                        error={
+                          touched.youthFosterCare &&
+                          errors.youthFosterCare !== undefined
+                        }
+                      />
+                      <Form.Radio
+                        name="youthFosterCare"
+                        value="No"
+                        label="No"
+                        onBlur={handleBlur}
+                        onChange={(_, { value }) =>
+                          setFieldValue("youthFosterCare", value)
+                        }
+                        checked={values.youthFosterCare === "No"}
+                        error={
+                          touched.youthFosterCare &&
+                          errors.youthFosterCare !== undefined
+                        }
+                      />
+                <b>Note: If the youth is in Foster Care but not in the care of Georgia, please provide the state name.</b>
+                <Form.Input
+                error={
+                  touched.youthFosterCareEntry &&
+                  errors.youthFosterCareEntry !== undefined && {
+                    content: errors.youthFosterCareEntry,
+                    pointing: "above",
+                  }
+                }
+                placeholder="Foster Care State Name"
+                name="youthFosterCareEntry"
+                value={values.youthFosterCareEntry}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                />
+            </Form.Group>
+            {hr}
             <Form.Button
               onClick={goToPrevPage}
               primary
---
+++ src/PageText/page12text.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const paragraph1 = (
+  <p style={{ textIndent: "2em" }}>
+    Boys and Girls Clubs of Metro Atlanta and the Georgia Division of Family and Children Services (DFCS) are partnering to provide
+valuable out-of-school programs for youth in Georgia. The information provided on this form will help ensure that eligible youth are
+benefiting from the partnership. <b>Please complete this form in its entirety and return it to the identified staff person at the
+program site. We thank you for your cooperation.</b> 
+  </p>
+);
+export const hr = (
+    <hr></hr>
+);
