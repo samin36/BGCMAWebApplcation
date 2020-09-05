52b6de19d1ac12f2b25d3838954375526a5a0153
pnamani3 <pnamani3@gatech.edu>
2020-09-05 13:25:47 -0400

    turning off validation

--- src/Components/Forms/Page10.js
+++ src/Components/Forms/Page10.js
@@ -89,7 +89,7 @@ const Page10 = ({ nextStep, prevStep, setFormStates }) => {
   return (
     <Formik
       initialValues={initialValues}
-      validationSchema={validationSchema}
+      //validationSchema={validationSchema}
       onSubmit={(values, { setSubmitting }) => {
         // as long as the current page isn't the one that submits the data, keep the stuff below
         setSubmitting(false);
