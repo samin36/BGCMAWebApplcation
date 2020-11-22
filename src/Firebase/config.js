517c6d68efd1722df2588355e6ce1bfb7255ddd6
Shrey Amin <samin36@gatech.edu>
2020-11-22 18:33:38 -0500

    master

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -1,9 +1,3 @@
 export default {
-  apiKey: "AIzaSyB-QqcBWsqiYKXYNkvGS036n7A89mQka9Q",
-  authDomain: "bgcmawebapplication.firebaseapp.com",
-  databaseURL: "https://bgcmawebapplication.firebaseio.com",
-  projectId: "bgcmawebapplication",
-  storageBucket: "bgcmawebapplication.appspot.com",
-  messagingSenderId: "763896089991",
-  appId: "1:763896089991:web:c41756bdc9904be19d9722",
+  // Paste the copied script contents here
 };
